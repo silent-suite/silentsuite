717ee3406c82ec0e3c4bbaa987509ca2e3711953
Tim Ross <info@silentsuite.io>
2026-09-06 13:23:12 +0000

    chore: prepare v0.5.6-beta release metadata

--- tests/test_release_versions_static.py
+++ tests/test_release_versions_static.py
@@ -10,8 +10,8 @@ import json
 import re
 
 ROOT = Path(__file__).resolve().parents[1]
-CURRENT_RELEASE_VERSION = "0.5.5-beta"
-CURRENT_ANDROID_VERSION_CODE = 19
+CURRENT_RELEASE_VERSION = "0.5.6-beta"
+CURRENT_ANDROID_VERSION_CODE = 20
 
 PACKAGE_JSON_PATHS = [
     "package.json",
